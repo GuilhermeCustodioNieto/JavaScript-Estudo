73a472080fc615c70683b476fbe2c8510838afaf
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-15 20:38:04 -0300

    The React study has been started

---
+++ react/Matheus Bastitti/meu-projeto/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  const name = "Guilherme";
+
+  return (
+    <div className="App">
+      <h1>Hello, world</h1>
+
+      <h1>Seu nome é: {name}</h1>
+    </div>
+  );
+}
+
+export default App;
