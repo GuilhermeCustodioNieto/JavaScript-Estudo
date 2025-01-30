87eaad4ca162693985260babb329e7508a9c2fb2
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-30 16:45:29 -0300

    Criado o consumo da API fake do projeto costs

--- react/Matheus Bastitti/costs/src/components/layouts/Container.js
+++ react/Matheus Bastitti/costs/src/components/layouts/Container.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import styles from './Container.module.css'
 function Container(props) {
-    console.log(props.children);
     
-
   return (
     <div className={`${styles.container} ${styles[props.customClass]}`}>
       {props.children}
--- react/Matheus Bastitti/costs/src/components/pages/NewProject.js
+++ react/Matheus Bastitti/costs/src/components/pages/NewProject.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
   return (
-    <div>
-      <h1>New Project</h1>
+    <div className={styles.newproject_container}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <p>Formulário</p>
+
+      <ProjectForm btnText="Criar Projeto"></ProjectForm>
     </div>
   )
 }
