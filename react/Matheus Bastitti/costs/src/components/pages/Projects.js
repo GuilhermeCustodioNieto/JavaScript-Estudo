997140fe0eae3ea17cf18bb069c0f982cf72ee9f
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-02-03 13:06:34 -0300

    Costs: Tela de projetos concluida

--- react/Matheus Bastitti/costs/src/components/pages/Projects.js
+++ react/Matheus Bastitti/costs/src/components/pages/Projects.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Message from '../layouts/Message'
 import { useLocation } from 'react-router-dom'
+import styles from  './Projects.module.css'
+import Container from '../layouts/Container'
+import LinkButton from '../layouts/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 function Projects() {
+
+  const [projects, setProjects] = useState([]);
 
   const location = useLocation()
 
@@ -11,16 +17,50 @@ function Projects() {
       message = location.state.message
     }
 
-    console.log(message);
-    
+    useEffect(() => {
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+          'Content-Type':'application/json'
+        }
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        
+        
+        setProjects(data)
+      })
+      .catch((err) => console.log(err))
+    }, [])
+    console.log(projects);
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+      </div>
 
       {message && (
         <Message msg={message} type="success"></Message>
       )}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard 
+            name={project.name}
+            id={project.id}
+            budget={project.budget}
+            category={project.category.name}
+            key={project.id}
+            
+            ></ProjectCard>
+            
+          ))}
+
+          
+      </Container>
+      
     </div>
   )
 }
